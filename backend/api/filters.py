0ff3ba059f372e5c078071b5fb181e152d61c481
Иван Ракшин <i.rakshin@yandex.ru>
2022-08-02 00:27:35 +0300

    работает

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,7 @@
 from django_filters import rest_framework as filters
 
 from recipes.models import Ingredient, Recipe
+from users.models import User
 
 
 class IngredientFilter(filters.FilterSet):
@@ -16,42 +17,22 @@ class IngredientFilter(filters.FilterSet):
 
 
 class RecipeFilter(filters.FilterSet):
-    """Фильтры для сортировки:
-    избранного
-    тегов
-    списка покупок."""
-
-    is_favorited = filters.BooleanFilter(
-        method='get_favorite',
-        label='favorite',
-    )
-    tags = filters.AllValuesMultipleFilter(
-        field_name='tags__slug',
-        label='tags',
-    )
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    author = filters.ModelChoiceFilter(queryset=User.objects.all())
+    is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
-        method='get_is_in_shopping_cart',
-        label='shopping_cart',
-    )
+        method='filter_is_in_shopping_cart')
+
+    def filter_is_favorited(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(favorites__user=self.request.user)
+        return queryset
+
+    def filter_is_in_shopping_cart(self, queryset, name, value):
+        if value and not self.request.user.is_anonymous:
+            return queryset.filter(cart__user=self.request.user)
+        return queryset
 
     class Meta:
         model = Recipe
-        fields = (
-            'tags',
-            'author',
-            'is_favorited',
-            'is_in_shopping_cart',
-        )
-
-    def get_favorite(self, queryset, name, value):
-        if value:
-            return queryset.filter(in_favorite__user=self.request.user)
-        return queryset.exclude(
-            in_favorite__user=self.request.user
-        )
-
-    def get_is_in_shopping_cart(self, queryset, name, value):
-        if value:
-            return Recipe.objects.filter(
-                shopping_recipe__user=self.request.user
-            )
+        fields = ('tags', 'author')
